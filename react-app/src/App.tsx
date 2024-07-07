a53bc02c0475b682929c4ebac327bb16ee500bec
Oksana Bondareva <oksana20-89@mail.ru>
2024-07-07 16:18:28 +0300

    feat: add api response

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,15 +1,36 @@
 import "./App.css";
 import React from "react";
 import SearchForm from "./components/SearchForm/SearchForm";
+import { Results } from "./components/Results/Results";
 
 class App extends React.Component {
+  state = {
+    searchValue: "",
+    dataApi: [],
+  };
+
+  componentDidMount = async () => {
+    try {
+      const response = await fetch(
+        `https://swapi.dev/api/people/?search=${this.state.searchValue}&page=1
+        `,
+      );
+      const data = await response.json();
+      this.setState({ dataApi: data.results });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <div>
         <section>
           <SearchForm />
         </section>
-        <section></section>
+        <section>
+          <Results data={this.state.dataApi} />
+        </section>
       </div>
     );
   }
--- react-app/src/components/Results/Results.tsx
+++ react-app/src/components/Results/Results.tsx
@@ -12,18 +12,39 @@ type ResultItem = {
   gender: string;
 };
 
-export class Results extends Component<{ item: ResultItem }> {
+type ResultItems = {
+  data: ResultItem[];
+};
+
+export class Results extends Component<ResultItems> {
   render() {
     return (
-      <div className="results-wrapper">
-        <p className="results-info">Name: {this.props.item.name}</p>
-        <p className="results-info">Mass: {this.props.item.mass}</p>
-        <p className="results-info">Height: {this.props.item.height}</p>
-        <p className="results-info">Hair color: {this.props.item.hair_color}</p>
-        <p className="results-info">Skin color: {this.props.item.skin_color}</p>
-        <p className="results-info">Eye color: {this.props.item.eye_color}</p>
-        <p className="results-info">Birth year: {this.props.item.birth_year}</p>
-        <p className="results-info">Gender: {this.props.item.gender}</p>
+      <div className="results-container">
+        {this.props.data.map(
+          (item: {
+            name: string;
+            mass: string;
+            height: string;
+            hair_color: string;
+            skin_color: string;
+            eye_color: string;
+            birth_year: string;
+            gender: string;
+          }) => {
+            return (
+              <div className="results-card">
+                <p className="results-info">Name: {item.name}</p>
+                <p className="results-info">Mass: {item.mass}</p>
+                <p className="results-info">Height: {item.height}</p>
+                <p className="results-info">Hair color: {item.hair_color}</p>
+                <p className="results-info">Skin color: {item.skin_color}</p>
+                <p className="results-info">Eye color: {item.eye_color}</p>
+                <p className="results-info">Birth year: {item.birth_year}</p>
+                <p className="results-info">Gender: {item.gender}</p>
+              </div>
+            );
+          },
+        )}
       </div>
     );
   }

1747327571f2f475286f706649806bc5d5b7e7ae
Oksana Bondareva <oksana20-89@mail.ru>
2024-07-10 17:25:04 +0300

    refactor: Conversion search form  of class components to functional components

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,40 +1,36 @@
-import { Component, ChangeEvent } from "react";
+import React, { useState, ChangeEvent } from "react";
 import "./SearchForm.css";
 
-interface SearchFormState {
+interface SearchFormProps {
   onSearch: (value: string) => void;
 }
 
-class SearchForm extends Component<SearchFormState> {
-  state = {
-    searchQuery: "",
+const SearchForm: React.FC<SearchFormProps> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
   };
 
-  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchQuery: event.target.value });
+  const handleSearch = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    onSearch(searchQuery);
   };
 
-  handleSearch = () => {
-    event?.preventDefault();
-    this.props.onSearch(this.state.searchQuery);
-  };
-
-  render() {
-    return (
-      <div className="search-form">
-        <input
-          className="search-form__input"
-          type="text"
-          placeholder="Search..."
-          value={this.state.searchQuery}
-          onChange={this.handleInputChange}
-        />
-        <button className="search-form__button" onClick={this.handleSearch}>
-          Search
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-form">
+      <input
+        className="search-form__input"
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
+      <button className="search-form__button" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+};
 
 export default SearchForm;

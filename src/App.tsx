5890e75b4ca633b4dfe8290fabb614214374cc18
Oksana Bondareva <oksana20-89@mail.ru>
2024-07-10 21:28:56 +0300

    refactor: Conversion App of class components to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,59 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-import React from "react";
 import SearchForm from "./components/SearchForm/SearchForm";
 import Results from "./components/Results/Results";
 
-class App extends React.Component {
-  state = {
-    searchValue: "",
-    dataApi: [],
-    isLoading: false,
-  };
+const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [dataApi, setDataApi] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  getSearch = async () => {
+  const getSearch = async () => {
     try {
-      this.setState({
-        isLoading: true,
-      });
+      setIsLoading(true);
       const response = await fetch(
-        `https://swapi.dev/api/people/?search=${this.state.searchValue}&page=1
-        `,
+        `https://swapi.dev/api/people/?search=${searchValue}&page=1`
       );
       const data = await response.json();
-      this.setState({ dataApi: data.results, isLoading: false });
+      setDataApi(data.results);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
 
-  componentDidMount = () => {
+  useEffect(() => {
     if (localStorage.getItem("search")) {
-      this.setState({ searchValue: localStorage.getItem("search") }, () => {
-        this.getSearch();
-      });
+      setSearchValue(localStorage.getItem("search") || "");
+      getSearch();
     } else {
-      this.getSearch();
+      getSearch();
     }
-  };
+  }, [searchValue]);
 
-  handleSearch = (query: string) => {
+  const handleSearch = (query: string) => {
     event?.preventDefault();
     localStorage.setItem("search", query);
-    this.setState({ searchValue: query }, () => {
-      this.getSearch();
-    });
+    setSearchValue(query);
   };
 
-  errorButtonHandler = () => {
-    this.setState(() => {
-      throw new Error("Oops, something went wrong...");
-    });
+  const errorButtonHandler = () => {
+    throw new Error("Oops, something went wrong...");
   };
 
-  render() {
-    return (
-      <div>
-        <section className="search-section">
-          <button className="error-button" onClick={this.errorButtonHandler}>
-            ERROR
-          </button>
-          <SearchForm onSearch={this.handleSearch} />
-        </section>
-        <section>
-          {this.state.isLoading ? (
-            <div className="loading">Loading...</div>
-          ) : (
-            <Results data={this.state.dataApi} />
-          )}
-        </section>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <section className="search-section">
+        <button className="error-button" onClick={errorButtonHandler}>
+          ERROR
+        </button>
+        <SearchForm onSearch={handleSearch} />
+      </section>
+      <section>
+        {isLoading ? <div className="loading">Loading...</div> : <Results data={dataApi} />}
+      </section>
+    </div>
+  );
+};
 
 export default App;

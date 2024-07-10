3771efd79037bc7054abd256aa81547620cd1b38
Oksana Bondareva <oksana20-89@mail.ru>
2024-07-11 01:16:53 +0300

    feat: add page pagination

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import SearchForm from "./components/SearchForm/SearchForm";
 import Results from "./components/Results/Results";
 
 const App = () => {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState(
+    localStorage.getItem("search") || "",
+  );
   const [dataApi, setDataApi] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const getSearch = async () => {
     try {
       setIsLoading(true);
       const response = await fetch(
-        `https://swapi.dev/api/people/?search=${searchValue}&page=1`
+        `https://swapi.dev/api/people/?search=${searchValue}&page=${currentPage}`,
       );
       const data = await response.json();
       setDataApi(data.results);
@@ -23,13 +26,9 @@ const App = () => {
   };
 
   useEffect(() => {
-    if (localStorage.getItem("search")) {
-      setSearchValue(localStorage.getItem("search") || "");
-      getSearch();
-    } else {
-      getSearch();
-    }
-  }, [searchValue]);
+    setSearchValue(localStorage.getItem("search") || "");
+    getSearch();
+  }, [searchValue, currentPage]);
 
   const handleSearch = (query: string) => {
     event?.preventDefault();
@@ -41,6 +40,16 @@ const App = () => {
     throw new Error("Oops, something went wrong...");
   };
 
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
   return (
     <div>
       <section className="search-section">
@@ -50,7 +59,29 @@ const App = () => {
         <SearchForm onSearch={handleSearch} />
       </section>
       <section>
-        {isLoading ? <div className="loading">Loading...</div> : <Results data={dataApi} />}
+        {isLoading ? (
+          <div className="loading">Loading...</div>
+        ) : (
+          <div className="results-wrapper">
+            <Results data={dataApi} />
+            {localStorage.getItem("search") === "" || "" ? (
+              <div className="pagination-pages">
+                <button
+                  className="pagination-button"
+                  onClick={handlePreviousPage}
+                >
+                  Prev
+                </button>
+                <span>Page {currentPage}</span>
+                <button className="pagination-button" onClick={handleNextPage}>
+                  Next
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        )}
       </section>
     </div>
   );

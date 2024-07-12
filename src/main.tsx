f974c1577f8e690c3dda8a5c82716f2c877b7ff8
Oksana Bondareva <oksana20-89@mail.ru>
2024-07-12 12:32:35 +0300

    feat: add routing for App and not found page

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./common/Router/Router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>,
 );

fd1e2c7bba8eaf05d42909b2784784002e505daf
Oksana Bondareva <oksanchik252@gmail.com>
2024-08-18 22:26:13 +0300

    feat: add indication for a newly entered data on the main route

--- src/Components/Cards/Cards.tsx
+++ src/Components/Cards/Cards.tsx
@@ -1,23 +1,51 @@
 import Card from "../Card/Card";
-import { CardData } from "../../Modal/Interfaces";
+import { CardData, FormData } from "../../Modal/Interfaces";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Store/Store";
 import "./Cards.css";
+import { useEffect, useState } from "react";
 
 export const Cards = () => {
   const cards = useSelector((state: RootState) => state.form.usersData);
+  const [highlightedUser, setHighlightedUser] = useState<FormData | null>(null);
+
+  useEffect(() => {
+    if (cards.length > 0) {
+      const newUser: FormData = cards[cards.length - 1];
+      setHighlightedUser(newUser);
+
+      const timer = setTimeout(() => {
+        setHighlightedUser(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [cards]);
 
   return cards.length > 0 ? (
     <div className="cards-wrapper">
       {cards.map((userCard: CardData, index: number) => {
         return (
-          <div className="card-container" key={`${index}`}>
+          <div
+            className="card-container"
+            key={`${index}`}
+            style={{
+              border:
+                userCard === highlightedUser
+                  ? "5px solid green"
+                  : "3px solid rgb(106, 14, 119)",
+              backgroundColor:
+                userCard === highlightedUser
+                  ? "lightgreen"
+                  : "rgb(233, 226, 239)",
+            }}
+          >
             <Card userCard={userCard} />
           </div>
         );
       })}
     </div>
   ) : (
-    <p>Not found cards</p>
+    <h1 className="not-found-card">Not found cards</h1>
   );
 };
